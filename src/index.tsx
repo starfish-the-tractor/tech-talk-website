af3b649b235c8ec189d6cdea812119be768bc7d3
Jyunmau <yuanmou8@gmail.com>
2020-03-26 23:48:50 +0800

    change to grommet

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import ThemeProvider, { Normalize } from "@fluent-ui/core/ThemeProvider";
+import Box from "@fluent-ui/core/Box";
+import { Grommet } from 'grommet';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <ThemeProvider>
+        <Normalize />
+        <Box padding={20} background="#CCC">
+          <Box acrylic={true} padding={[15, 25]}>
+            Acrylic
+          </Box>
+        </Box>
+      </ThemeProvider>
+    );
+  }
+}
+
+function App(){
+  return(
+    <HelloMessage />
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
